d40a246f908ed975453c1f26bae759bd03ee86ea
kadir12386 <kadir12386@gmail.com>
2021-11-11 09:05:09 +0300

    err

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
+// import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-
 import { BrowserRouter as Router } from "react-router-dom";
+const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <App />
     </Router>
   </React.StrictMode>,
-  document.getElementById("root")
+  rootElement
 );
